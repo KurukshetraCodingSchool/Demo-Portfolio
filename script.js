611bbbef099079537c444450392b3427ffa58ed7
kurukshetra <kurukshetracodingschool.com>
2025-06-11 18:47:14 +0530

    Counter Update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+const counters = [
+    { selector: ".experience", end: 3 },
+    { selector: ".projects", end: 20 },
+    { selector: ".students", end: 150 }
+  ];
+
+  counters.forEach(counter => {
+    let obj = { val: 0 };
+    gsap.to(obj, {
+      val: counter.end,
+      duration: 2,
+      ease: "power1.out",
+      onUpdate: () => {
+        const el = document.querySelector(counter.selector);
+        el.textContent = Math.floor(obj.val);
+      }
+    });
+  });
